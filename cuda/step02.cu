b72ba2622a4ae84a0ecbeb3e78fe59245a5a19ac
Rio Yokota <rioyokota@gmail.com>
2017-04-30 18:24:42 +0900

    Add CUDA examples

---
+++ cuda/step02.cu
@@ -0,0 +1,31 @@
+#include <cstdio>
+
+__global__ void add(int *a, int *b, int *c) {
+  *c = *a + *b;
+}
+
+int main(void) {
+  int a, b, c;
+  int *d_a, *d_b, *d_c;
+  int size = sizeof(int);
+
+  cudaMalloc((void **) &d_a, size);
+  cudaMalloc((void **) &d_b, size);
+  cudaMalloc((void **) &d_c, size);
+
+  a = 2;
+  b = 7;
+
+  cudaMemcpy(d_a, &a, size, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_b, &b, size, cudaMemcpyHostToDevice);
+
+  add<<<1,1>>>(d_a, d_b, d_c);
+
+  cudaMemcpy(&c, d_c, size, cudaMemcpyDeviceToHost);
+
+  printf("%d\n",c);
+
+  cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+
+  return 0;
+}
