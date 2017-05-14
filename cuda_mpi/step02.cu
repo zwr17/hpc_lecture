cafd25d275ce3a3015166bcb3444a5698c8cb405
Rio Yokota <rioyokota@gmail.com>
2017-05-15 08:20:23 +0900

    Add CUDA MPI codes

--- cuda_mpi/step02.cu
+++ cuda_mpi/step02.cu
@@ -7,16 +7,19 @@ __global__ void GPU_Kernel() {
 }
 
 int main(int argc, char **argv) {
-  int mpisize, mpirank, gpusize, gpurank;
+  char hostname[256];
+  int mpisize, mpirank, gpusize, gpurank, len;
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
   MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
+  MPI_Get_processor_name(hostname, &len);
   cudaGetDeviceCount(&gpusize);
   cudaSetDevice(mpirank % gpusize);
   cudaGetDevice(&gpurank);
   for (int irank=0; irank<mpisize; irank++) {
     MPI_Barrier(MPI_COMM_WORLD);
     if (mpirank == irank) {
+      printf("Hostname    : %s\n", hostname);
       printf("MPI rank    : %d / %d  GPU device : %d / %d\n",
              mpirank, mpisize, gpurank, gpusize);
       GPU_Kernel<<<2,2>>>();
