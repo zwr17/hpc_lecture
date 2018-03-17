707e42c9ad1d03443bb50a0b8cbbcb55b94dfaa3
Rio Yokota <rioyokota@gmail.com>
2018-03-17 17:49:47 +0300

    Simplify producer consumer

--- pthreads/10_buffer.cpp
+++ pthreads/10_buffer.cpp
@@ -7,13 +7,20 @@ const int loops=2;         // number of producer loops
 const int producers=2;     // number of producers
 const int consumers=2;     // number of consumers
 const int num_threads=producers+consumers;
-int value=EMPTY;           // the value
+const int max_buffer=3;    // maximum capacity of buffer
+int begin_p=0;             // tracks where next consume should come from
+int end_p=0;               // tracks where next produce should go to
+int count=0;               // counts how many entries are full
+int *buffer;               // the buffer itself: malloc in main()
 pthread_cond_t empty=PTHREAD_COND_INITIALIZER;
 pthread_cond_t fill=PTHREAD_COND_INITIALIZER;
 pthread_mutex_t mutex=PTHREAD_MUTEX_INITIALIZER;
 
 void print_headers(int producers, int consumers) {
-  printf("    ");
+  printf("N   ");
+  for (int i=0; i<max_buffer; i++) {
+    printf("   ");
+  }
   for (int i=0; i<producers; i++)
     printf(" P%d  ", i);
   for (int i=0; i<consumers; i++)
@@ -22,13 +29,24 @@ void print_headers(int producers, int consumers) {
 }
 
 void print(int thread_id, const char *str) {
-  printf("[");
-  if (value == EMPTY) {
-    printf("%s", "-");
-  } else if (value == END) {
-    printf("%s", "E");
-  } else {
-    printf("%d", value);
+  printf("%d [", count);
+  for (int i=0; i<max_buffer; i++) {
+    if (begin_p == i && end_p == i) {
+      printf("*");
+    } else if (begin_p == i) {
+      printf("b");
+    } else if (end_p == i) {
+      printf("e");
+    } else {
+      printf(" ");
+    }
+    if (buffer[i] == EMPTY) {
+      printf("%s ", "-");
+    } else if (buffer[i] == END) {
+      printf("%s ", "E");
+    } else {
+      printf("%d ", buffer[i]);
+    }
   }
   printf("] ");
   for (int i=0; i<thread_id; i++) {
@@ -37,13 +55,17 @@ void print(int thread_id, const char *str) {
   printf("%4s\n", str);
 }
 
-void put(int v) {
-  value=v;
+void put(int value) {
+  buffer[end_p]=value;
+  end_p=(end_p + 1) % max_buffer;
+  count++;
 }
 
 int get() {
-  int tmp=value;
-  value=EMPTY;
+  int tmp=buffer[begin_p];
+  buffer[begin_p]=EMPTY;
+  begin_p=(begin_p + 1) % max_buffer;
+  count--;
   return tmp;
 }
 
@@ -52,7 +74,7 @@ void *producer(void *arg) {
   int base=id * loops;
   for (int i=0; i<loops; i++) {
     pthread_mutex_lock(&mutex); print(id, "lock");
-    while (value != EMPTY) { print(id, "full"); print(id, "uloc");
+    while (count == max_buffer) { print(id, "full"); print(id, "uloc");
       pthread_cond_wait(&empty, &mutex); print(id, "resu"); print(id, "lock");
     }
     put(base + i); print(id, "put ");
@@ -67,7 +89,7 @@ void *consumer(void *arg) {
   int tmp=0;
   while (tmp != END) {
     pthread_mutex_lock(&mutex); print(id, "lock");
-    while (value == EMPTY) { print(id, "none"); print(id, "uloc");
+    while (count == 0) { print(id, "none"); print(id, "uloc");
       pthread_cond_wait(&fill, &mutex); print(id, "resu"); print(id, "lock");
     }
     tmp=get(); print(id, "get ");
@@ -78,6 +100,10 @@ void *consumer(void *arg) {
 }
 
 int main(int argc, char *argv[]) {
+  buffer=new int [max_buffer];
+  for (int i=0; i<max_buffer; i++) {
+    buffer[i]=EMPTY;
+  }
   print_headers(producers, consumers);
   pthread_t pid[num_threads], cid[num_threads];
   size_t thread_id=0;
@@ -94,7 +120,7 @@ int main(int argc, char *argv[]) {
   }
   for (int i=0; i<consumers; i++) {
     pthread_mutex_lock(&mutex);
-    while (value != EMPTY)
+    while (count == max_buffer)
       pthread_cond_wait(&empty, &mutex);
     put(END);
     pthread_cond_signal(&fill);
@@ -103,5 +129,6 @@ int main(int argc, char *argv[]) {
   for (int i=0; i<consumers; i++) {
     pthread_join(cid[i], NULL);
   }
+  delete[] buffer;
   return 0;
 }
