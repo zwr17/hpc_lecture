fdb44f2906139ca3f00b5799220e0a52958ac673
Rio Yokota <rioyokota@gmail.com>
2018-04-17 20:43:11 +0900

    Update cuda

---
+++ cuda/00_hello.cu
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+__global__ void print(void) {
+  printf("Hello GPU\n");
+}
+
+int main() {
+  printf("Hello CPU\n");
+  print<<<1,1>>>();
+  cudaDeviceSynchronize();
+  return 0;
+}
---
+++ cuda/01_memcpy.cu
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+__global__ void kernel(float *a) {
+  a[threadIdx.x] = threadIdx.x;
+}
+
+int main(void) {
+  const int size = 4 * sizeof(float);
+  float *a; *b = (float*) malloc(size);
+  cudaMalloc(&a, size);
+  kernel<<<1,4>>>(a);
+  cudaMemcpy(b, a, size, cudaMemcpyDeviceToHost);
+  for (int i=0; i<4; i++) printf("%f\n",b[i]);
+  cudaFree(a);
+  free(b);
+  return 0;
+}
