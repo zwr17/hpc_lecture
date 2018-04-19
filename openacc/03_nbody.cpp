207a3f90ef2f51eb7be53941a018ff7923d512f0
Rio Yokota <rioyokota@gmail.com>
2018-04-19 20:32:17 +0900

    update 03_nbody

--- openacc/03_nbody.cpp
+++ openacc/03_nbody.cpp
@@ -28,41 +28,44 @@ int main() {
   }
   printf("N      : %d\n",N);
 // CUDA
-  gettimeofday(&tic, NULL);
-#pragma acc kernels loop gang(N/threads) vector(threads)
-  for (int i=0; i<N; i++) {
-    float pi = 0;
-    float axi = 0;
-    float ayi = 0;
-    float azi = 0;
-    float xi = x[i];
-    float yi = y[i];
-    float zi = z[i];
-    for (int j=0; j<N; j++) {
-      float dx = x[j] - xi;
-      float dy = y[j] - yi;
-      float dz = z[j] - zi;
-      float R2 = dx * dx + dy * dy + dz * dz + EPS2;
-      float invR = 1.0f / sqrtf(R2);
-      float invR3 = m[j] * invR * invR * invR;
-      pi += m[j] * invR;
-      axi += dx * invR3;
-      ayi += dy * invR3;
-      azi += dz * invR3;
+#pragma acc data copyin(x[:N],y[:N],z[:N],m[:N]) copyout(p[:N],ax[:N],ay[:N],az[:N])
+  {
+    gettimeofday(&tic, NULL);
+#pragma acc kernels loop independent gang(N/threads) vector(threads)
+    for (int i=0; i<N; i++) {
+      float pi = 0;
+      float axi = 0;
+      float ayi = 0;
+      float azi = 0;
+      float xi = x[i];
+      float yi = y[i];
+      float zi = z[i];
+      for (int j=0; j<N; j++) {
+        float dx = x[j] - xi;
+        float dy = y[j] - yi;
+        float dz = z[j] - zi;
+        float R2 = dx * dx + dy * dy + dz * dz + EPS2;
+        float invR = 1.0f / sqrtf(R2);
+        float invR3 = m[j] * invR * invR * invR;
+        pi += m[j] * invR;
+        axi += dx * invR3;
+        ayi += dy * invR3;
+        azi += dz * invR3;
+      }
+      p[i] = pi;
+      ax[i] = axi;
+      ay[i] = ayi;
+      az[i] = azi;
     }
-    p[i] = pi;
-    ax[i] = axi;
-    ay[i] = ayi;
-    az[i] = azi;
+    gettimeofday(&toc, NULL);
   }
-  gettimeofday(&toc, NULL);
   double time = toc.tv_sec-tic.tv_sec+(toc.tv_usec-tic.tv_usec)*1e-6;
   printf("CUDA   : %e s : %lf GFlops\n",time, OPS/time);
 
 // No CUDA
   float pdiff = 0, pnorm = 0, adiff = 0, anorm = 0;
   gettimeofday(&tic, NULL);
-#pragma omp parallel for private(j) reduction(+: pdiff, pnorm, adiff, anorm)
+#pragma omp parallel for reduction(+: pdiff, pnorm, adiff, anorm)
   for (int i=0; i<N; i++) {
     float pi = 0;
     float axi = 0;
