28d2f302e238d1956070f9aa7abcaaffaf31eadb
Rio Yokota <rioyokota@gmail.com>
2018-04-24 17:40:52 +0900

    Update cuda-mpi hello

--- cuda_mpi/00_hello.cu
+++ cuda_mpi/00_hello.cu
@@ -2,6 +2,7 @@
 #include <cstdio>
 
 __global__ void mykernel(void) {
+  printf("Hello GPU\n");
 }
 
 int main(int argc, char ** argv) {
@@ -10,7 +11,7 @@ int main(int argc, char ** argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
   MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
   mykernel<<<1,1>>>();
+  cudaDeviceSynchronize();
   printf("rank: %d/%d\n",mpirank,mpisize);
   MPI_Finalize();
 }
-
