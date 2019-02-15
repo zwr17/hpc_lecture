4ab099f4e402de8376b51a59ae46fecef0ea3d0c
Rio Yokota <rioyokota@gmail.com>
2019-02-15 20:44:43 +0900

    Add thrust

---
+++ cuda/11_thrust_sum.cu
@@ -0,0 +1,11 @@
+#include <thrust/device_vector.h>
+#include <iostream>
+
+int main(void) {
+  thrust::device_vector<int> a(4);
+  thrust::sequence(a.begin(), a.end());
+  for(int i=0; i<a.size(); i++)
+    std::cout << a[i] << std::endl;
+  int sum = thrust::reduce(a.begin(), a.end());
+  std::cout << sum << std::endl;
+}
