4f26a4c821505c1d46c8c581b9d522f15af165bf
Rio Yokota <rioyokota@gmail.com>
2019-05-15 20:50:57 +0900

    update petsc

--- solver/02_petsc.c
+++ solver/02_petsc.c
@@ -1,155 +1,204 @@
-/*   DMDA/KSP solving a system of linear equations.
-     Poisson equation in 2D:
-
-     div(grad p) = f,  0 < x,y < 1
-     with
-       forcing function f = -cos(m*pi*x)*cos(n*pi*y),
-       Neuman boundary conditions
-        dp/dx = 0 for x = 0, x = 1.
-        dp/dy = 0 for y = 0, y = 1.
-
-     Example of Usage:
-          mpiexec -n 4 ./a.out -da_grid_x 3 -da_grid_y 3 -pc_type mg -da_refine 10 -ksp_monitor -ksp_view -log_view
-*/
-
-static char help[] = "Solves 2D Poisson equation using multigrid.\n\n";
-
-#include <petscdm.h>
-#include <petscdmda.h>
 #include <petscksp.h>
-#include <petscsys.h>
-#include <petscvec.h>
 
-extern PetscErrorCode ComputeJacobian(KSP,Mat,Mat,void*);
-extern PetscErrorCode ComputeRHS(KSP,Vec,void*);
-
-typedef struct {
-  PetscScalar uu, tt;
-} UserContext;
-
-int main(int argc,char **argv)
+int main(int argc,char **args)
 {
-  KSP            ksp;
-  DM             da;
-  UserContext    user;
+  Vec            x, b, u;      /* approx solution, RHS, exact solution */
+  Mat            A;            /* linear system matrix */
+  KSP            ksp;          /* linear solver context */
+  PC             pc;           /* preconditioner context */
+  PetscReal      norm;         /* norm of solution error */
   PetscErrorCode ierr;
+  PetscInt       i,n = 10,col[3],its;
+  PetscMPIInt    size;
+  PetscScalar    one = 1.0,value[3];
+  PetscBool      nonzeroguess = PETSC_FALSE,changepcside = PETSC_FALSE;
 
-  ierr = PetscInitialize(&argc,&argv,(char*)0,help);if (ierr) return ierr;
+  static char help[] = "Solves a tridiagonal linear system with KSP.\n\n";
+  ierr = PetscInitialize(&argc,&args,(char*)0,help);if (ierr) return ierr;
+  ierr = MPI_Comm_size(PETSC_COMM_WORLD,&size);CHKERRQ(ierr);
+  if (size != 1) SETERRQ(PETSC_COMM_WORLD,1,"This is a uniprocessor example only!");
+  ierr = PetscOptionsGetInt(NULL,NULL,"-n",&n,NULL);CHKERRQ(ierr);
+  ierr = PetscOptionsGetBool(NULL,NULL,"-nonzero_guess",&nonzeroguess,NULL);CHKERRQ(ierr);
+
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+         Compute the matrix and right-hand-side vector that define
+         the linear system, Ax = b.
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+  /*
+     Create vectors.  Note that we form 1 vector from scratch and
+     then duplicate as needed.
+  */
+  ierr = VecCreate(PETSC_COMM_WORLD,&x);CHKERRQ(ierr);
+  ierr = PetscObjectSetName((PetscObject) x, "Solution");CHKERRQ(ierr);
+  ierr = VecSetSizes(x,PETSC_DECIDE,n);CHKERRQ(ierr);
+  ierr = VecSetFromOptions(x);CHKERRQ(ierr);
+  ierr = VecDuplicate(x,&b);CHKERRQ(ierr);
+  ierr = VecDuplicate(x,&u);CHKERRQ(ierr);
+
+  /*
+     Create matrix.  When using MatCreate(), the matrix format can
+     be specified at runtime.
+
+     Performance tuning note:  For problems of substantial size,
+     preallocation of matrix memory is crucial for attaining good
+     performance. See the matrix chapter of the users manual for details.
+  */
+  ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
+  ierr = MatSetSizes(A,PETSC_DECIDE,PETSC_DECIDE,n,n);CHKERRQ(ierr);
+  ierr = MatSetFromOptions(A);CHKERRQ(ierr);
+  ierr = MatSetUp(A);CHKERRQ(ierr);
+
+  /*
+     Assemble matrix
+  */
+  value[0] = -1.0; value[1] = 2.0; value[2] = -1.0;
+  for (i=1; i<n-1; i++) {
+    col[0] = i-1; col[1] = i; col[2] = i+1;
+    ierr   = MatSetValues(A,1,&i,3,col,value,INSERT_VALUES);CHKERRQ(ierr);
+  }
+  i    = n - 1; col[0] = n - 2; col[1] = n - 1;
+  ierr = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
+  i    = 0; col[0] = 0; col[1] = 1; value[0] = 2.0; value[1] = -1.0;
+  ierr = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
+  ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+  ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+
+  /*
+     Set exact solution; then compute right-hand-side vector.
+  */
+  ierr = VecSet(u,one);CHKERRQ(ierr);
+  ierr = MatMult(A,u,b);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+                Create the linear solver and set various options
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  /*
+     Create linear solver context
+  */
   ierr = KSPCreate(PETSC_COMM_WORLD,&ksp);CHKERRQ(ierr);
-  ierr = DMDACreate2d(PETSC_COMM_WORLD, DM_BOUNDARY_NONE, DM_BOUNDARY_NONE,DMDA_STENCIL_STAR,11,11,PETSC_DECIDE,PETSC_DECIDE,1,1,NULL,NULL,&da);CHKERRQ(ierr);
-  ierr = DMSetFromOptions(da);CHKERRQ(ierr);
-  ierr = DMSetUp(da);CHKERRQ(ierr);
-  ierr = KSPSetDM(ksp,(DM)da);CHKERRQ(ierr);
-  ierr = DMSetApplicationContext(da,&user);CHKERRQ(ierr);
 
-  user.uu     = 1.0;
-  user.tt     = 1.0;
+  /*
+     Set operators. Here the matrix that defines the linear system
+     also serves as the preconditioning matrix.
+  */
+  ierr = KSPSetOperators(ksp,A,A);CHKERRQ(ierr);
 
-  ierr = KSPSetComputeRHS(ksp,ComputeRHS,&user);CHKERRQ(ierr);
-  ierr = KSPSetComputeOperators(ksp,ComputeJacobian,&user);CHKERRQ(ierr);
+  /*
+     Test if you can change the KSP side and type after they have been previously set
+  */
+  ierr = PetscOptionsGetBool(NULL,NULL,"-change_pc_side",&changepcside,NULL);CHKERRQ(ierr);
+  if (changepcside) {
+    ierr = KSPSetUp(ksp);CHKERRQ(ierr);
+    ierr = PetscOptionsInsertString(NULL,"-ksp_norm_type unpreconditioned -ksp_pc_side right");CHKERRQ(ierr);
+    ierr = KSPSetFromOptions(ksp);CHKERRQ(ierr);
+    ierr = KSPSetUp(ksp);CHKERRQ(ierr);
+  }
+
+  /*
+     Set linear solver defaults for this problem (optional).
+     - By extracting the KSP and PC contexts from the KSP context,
+       we can then directly call any KSP and PC routines to set
+       various options.
+     - The following four statements are optional; all of these
+       parameters could alternatively be specified at runtime via
+       KSPSetFromOptions();
+  */
+  ierr = KSPGetPC(ksp,&pc);CHKERRQ(ierr);
+  ierr = PCSetType(pc,PCJACOBI);CHKERRQ(ierr);
+  ierr = KSPSetTolerances(ksp,1.e-5,PETSC_DEFAULT,PETSC_DEFAULT,PETSC_DEFAULT);CHKERRQ(ierr);
+
+  /*
+    Set runtime options, e.g.,
+        -ksp_type <type> -pc_type <type> -ksp_monitor -ksp_rtol <rtol>
+    These options will override those specified above as long as
+    KSPSetFromOptions() is called _after_ any other customization
+    routines.
+  */
   ierr = KSPSetFromOptions(ksp);CHKERRQ(ierr);
-  ierr = KSPSolve(ksp,NULL,NULL);CHKERRQ(ierr);
 
-  ierr = DMDestroy(&da);CHKERRQ(ierr);
+  if (nonzeroguess) {
+    PetscScalar p = .5;
+    ierr = VecSet(x,p);CHKERRQ(ierr);
+    ierr = KSPSetInitialGuessNonzero(ksp,PETSC_TRUE);CHKERRQ(ierr);
+  }
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+                      Solve the linear system
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  /*
+     Solve linear system
+  */
+  ierr = KSPSolve(ksp,b,x);CHKERRQ(ierr);
+
+  /*
+     View solver info; we could instead use the option -ksp_view to
+     print this info to the screen at the conclusion of KSPSolve().
+  */
+  ierr = KSPView(ksp,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+
+  /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+                      Check solution and clean up
+     - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+  /*
+     Check the error
+  */
+  ierr = VecAXPY(x,-1.0,u);CHKERRQ(ierr);
+  ierr = VecNorm(x,NORM_2,&norm);CHKERRQ(ierr);
+  ierr = KSPGetIterationNumber(ksp,&its);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"Norm of error %g, Iterations %D\n",(double)norm,its);CHKERRQ(ierr);
+
+  /*
+     Free work space.  All PETSc objects should be destroyed when they
+     are no longer needed.
+  */
+  ierr = VecDestroy(&x);CHKERRQ(ierr); ierr = VecDestroy(&u);CHKERRQ(ierr);
+  ierr = VecDestroy(&b);CHKERRQ(ierr); ierr = MatDestroy(&A);CHKERRQ(ierr);
   ierr = KSPDestroy(&ksp);CHKERRQ(ierr);
+
+  /*
+     Always call PetscFinalize() before exiting a program.  This routine
+       - finalizes the PETSc libraries as well as MPI
+       - provides summary and diagnostic information if certain runtime
+         options are chosen (e.g., -log_view).
+  */
   ierr = PetscFinalize();
   return ierr;
 }
 
-PetscErrorCode ComputeRHS(KSP ksp,Vec b,void *ctx)
-{
-  UserContext    *user = (UserContext*)ctx;
-  PetscErrorCode ierr;
-  PetscInt       i,j,M,N,xm,ym,xs,ys;
-  PetscScalar    Hx,Hy,pi,uu,tt;
-  PetscScalar    **array;
-  DM             da;
-  MatNullSpace   nullspace;
 
-  PetscFunctionBeginUser;
-  ierr = KSPGetDM(ksp,&da);CHKERRQ(ierr);
-  ierr = DMDAGetInfo(da, 0, &M, &N, 0,0,0,0,0,0,0,0,0,0);CHKERRQ(ierr);
-  uu   = user->uu; tt = user->tt;
-  pi   = 4*atan(1.0);
-  Hx   = 1.0/(PetscReal)(M);
-  Hy   = 1.0/(PetscReal)(N);
+/*TEST
 
-  ierr = DMDAGetCorners(da,&xs,&ys,0,&xm,&ym,0);CHKERRQ(ierr); /* Fine grid */
-  ierr = DMDAVecGetArray(da, b, &array);CHKERRQ(ierr);
-  for (j=ys; j<ys+ym; j++) {
-    for (i=xs; i<xs+xm; i++) {
-      array[j][i] = -PetscCosScalar(uu*pi*((PetscReal)i+0.5)*Hx)*PetscCosScalar(tt*pi*((PetscReal)j+0.5)*Hy)*Hx*Hy;
-    }
-  }
-  ierr = DMDAVecRestoreArray(da, b, &array);CHKERRQ(ierr);
-  ierr = VecAssemblyBegin(b);CHKERRQ(ierr);
-  ierr = VecAssemblyEnd(b);CHKERRQ(ierr);
+   test:
+      args: -ksp_monitor_short -ksp_gmres_cgs_refinement_type refine_always
 
-  /* force right hand side to be consistent for singular matrix */
-  /* note this is really a hack, normally the model would provide you with a consistent right handside */
-  ierr = MatNullSpaceCreate(PETSC_COMM_WORLD,PETSC_TRUE,0,0,&nullspace);CHKERRQ(ierr);
-  ierr = MatNullSpaceRemove(nullspace,b);CHKERRQ(ierr);
-  ierr = MatNullSpaceDestroy(&nullspace);CHKERRQ(ierr);
-  PetscFunctionReturn(0);
-}
+   test:
+      suffix: 2
+      args: -pc_type sor -pc_sor_symmetric -ksp_monitor_short -ksp_gmres_cgs_refinement_type refine_always
 
-PetscErrorCode ComputeJacobian(KSP ksp,Mat J, Mat jac,void *ctx)
-{
-  PetscErrorCode ierr;
-  PetscInt       i, j, M, N, xm, ym, xs, ys, num, numi, numj;
-  PetscScalar    v[5], Hx, Hy, HydHx, HxdHy;
-  MatStencil     row, col[5];
-  DM             da;
-  MatNullSpace   nullspace;
+   test:
+      suffix: 2_aijcusparse
+      requires: veccuda
+      args: -pc_type sor -pc_sor_symmetric -ksp_monitor_short -ksp_gmres_cgs_refinement_type refine_always -mat_type aijcusparse -vec_type cuda
 
-  PetscFunctionBeginUser;
-  ierr  = KSPGetDM(ksp,&da);CHKERRQ(ierr);
-  ierr  = DMDAGetInfo(da,0,&M,&N,0,0,0,0,0,0,0,0,0,0);CHKERRQ(ierr);
-  Hx    = 1.0 / (PetscReal)(M);
-  Hy    = 1.0 / (PetscReal)(N);
-  HxdHy = Hx/Hy;
-  HydHx = Hy/Hx;
-  ierr  = DMDAGetCorners(da,&xs,&ys,0,&xm,&ym,0);CHKERRQ(ierr);
-  for (j=ys; j<ys+ym; j++) {
-    for (i=xs; i<xs+xm; i++) {
-      row.i = i; row.j = j;
+   test:
+      suffix: 3
+      args: -pc_type eisenstat -ksp_monitor_short -ksp_gmres_cgs_refinement_type refine_always
 
-      if (i==0 || j==0 || i==M-1 || j==N-1) {
-        num=0; numi=0; numj=0;
-        if (j!=0) {
-          v[num] = -HxdHy;              col[num].i = i;   col[num].j = j-1;
-          num++; numj++;
-        }
-        if (i!=0) {
-          v[num] = -HydHx;              col[num].i = i-1; col[num].j = j;
-          num++; numi++;
-        }
-        if (i!=M-1) {
-          v[num] = -HydHx;              col[num].i = i+1; col[num].j = j;
-          num++; numi++;
-        }
-        if (j!=N-1) {
-          v[num] = -HxdHy;              col[num].i = i;   col[num].j = j+1;
-          num++; numj++;
-        }
-        v[num] = ((PetscReal)(numj)*HxdHy + (PetscReal)(numi)*HydHx); col[num].i = i;   col[num].j = j;
-        num++;
-        ierr = MatSetValuesStencil(jac,1,&row,num,col,v,INSERT_VALUES);CHKERRQ(ierr);
-      } else {
-        v[0] = -HxdHy;              col[0].i = i;   col[0].j = j-1;
-        v[1] = -HydHx;              col[1].i = i-1; col[1].j = j;
-        v[2] = 2.0*(HxdHy + HydHx); col[2].i = i;   col[2].j = j;
-        v[3] = -HydHx;              col[3].i = i+1; col[3].j = j;
-        v[4] = -HxdHy;              col[4].i = i;   col[4].j = j+1;
-        ierr = MatSetValuesStencil(jac,1,&row,5,col,v,INSERT_VALUES);CHKERRQ(ierr);
-      }
-    }
-  }
-  ierr = MatAssemblyBegin(jac,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
-  ierr = MatAssemblyEnd(jac,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
+   test:
+      suffix: 3_aijcusparse
+      requires: veccuda
+      args: -pc_type eisenstat -ksp_monitor_short -ksp_gmres_cgs_refinement_type refine_always -mat_type aijcusparse -vec_type cuda
 
-  ierr = MatNullSpaceCreate(PETSC_COMM_WORLD,PETSC_TRUE,0,0,&nullspace);CHKERRQ(ierr);
-  ierr = MatSetNullSpace(J,nullspace);CHKERRQ(ierr);
-  ierr = MatNullSpaceDestroy(&nullspace);CHKERRQ(ierr);
-  PetscFunctionReturn(0);
-}
+   test:
+      suffix: aijcusparse
+      requires: veccuda
+      args: -ksp_monitor_short -ksp_gmres_cgs_refinement_type refine_always -mat_type aijcusparse -vec_type cuda
+      output_file: output/ex1_1_aijcusparse.out
+
+   test:
+      suffix: changepcside
+      args: -ksp_monitor_short -ksp_gmres_cgs_refinement_type refine_always -change_pc_side
+
+TEST*/
