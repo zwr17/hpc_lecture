445e5d19b0b0a8dccc5fdc48f19595ad4a947286
Rio Yokota <rioyokota@gmail.com>
2020-03-20 18:43:00 +0900

    Reorder openmp problems

--- mpi/00_hello.cpp
+++ mpi/00_hello.cpp
@@ -1,12 +1,11 @@
+#include <iostream>
 #include "mpi.h"
-#include <cstdio>
-int main() {
-  int argc;
-  char ** argv;
+
+int main(int argc, char** argv) {
   MPI_Init(&argc, &argv);
   int mpisize, mpirank;
   MPI_Comm_size(MPI_COMM_WORLD, &mpisize);
   MPI_Comm_rank(MPI_COMM_WORLD, &mpirank);
-  printf("rank: %d/%d\n",mpirank,mpisize);
+  std::cout << "rank: " << mpirank << "/" << mpisize << std::endl;
   MPI_Finalize();
 }
