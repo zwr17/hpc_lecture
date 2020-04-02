c081adb7a273d97488a7b52ee432fd89ef7788b0
Rio Yokota <rioyokota@gmail.com>
2020-04-02 18:14:05 +0900

    Add function SIMD

---
+++ simd/03_function.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void add(float a, float b, float &c) {
+  c = a + b;
+}
+
+int main() {
+  const int N = 16;
+  float a[N], b[N], c[N];
+  for(int i=0; i<N; i++) {
+    a[i] = i;
+    b[i] = i * 0.1;
+    c[i] = 0;
+  }
+  for(int i=0; i<N; i++)
+    add(a[i],b[i],c[i]);
+  for(int i=0; i<N; i++)
+    std::cout << i << " " << c[i] << std::endl;
+}
