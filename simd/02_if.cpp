94e2f909464ee6bbbdbff10053f59d067daf71ab
Rio Yokota <rioyokota@gmail.com>
2020-04-02 15:34:44 +0900

    SIMD if

--- simd/02_if.cpp
+++ simd/02_if.cpp
@@ -8,8 +8,10 @@ int main() {
     b[i] = i * 0.1;
     c[i] = 0;
   }
-  for(int i=0; i<N; i++)
-    c[i] = a[i] + b[i];
+  for(int i=0; i<N; i++) {
+    if(a[i] < 10)
+      c[i] = a[i] + b[i];
+  }
   for(int i=0; i<N; i++)
     std::cout << i << " " << c[i] << std::endl;
 }
