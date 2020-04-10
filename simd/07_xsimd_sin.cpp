1c82e7b8dc19d601dc2af0fb328f1b5076b088d3
Rio Yokota <rioyokota@gmail.com>
2020-04-10 17:43:38 +0900

    remove xsimd

---
+++ simd/07_xsimd_sin.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <cmath>
+#include "xsimd/xsimd.hpp"
+
+int main() {
+  const int N = 8;
+  xsimd::batch<float, N> a, b;
+  for(int i=0; i<N; i++)
+    a[i] = i * M_PI / (N - 1);
+  b = sin(a);
+  for(int i=0; i<N; i++)
+    printf("%g %g\n",a[i],b[i]);
+}
