234b268b9003a0204c1bd414e50e6764e718cf7d
Rio Yokota <rioyokota@gmail.com>
2020-04-15 08:46:09 +0900

    SIMD align

--- simd/13_unaligned_vecadd.cpp
+++ simd/13_unaligned_vecadd.cpp
@@ -2,16 +2,21 @@
 
 int main() {
   const int N = 8;
-  float a[N], b[N], c[N];
+  float *a = new float [N];
+  float *b = new float [N];
+  float *c = new float [N];
   for(int i=0; i<N; i++) {
     a[i] = i;
-    b[i] = i * 0.1;
+    b[i] = i * 0.01;
     c[i] = 0;
   }
-  //asm volatile ("# begin loop");
+  asm volatile ("# begin loop");
   for(int i=0; i<N; i++)
     c[i] = a[i] + b[i];
-  //asm volatile ("# end loop");
+  asm volatile ("# end loop");
   for(int i=0; i<N; i++)
     printf("%d %g\n",i,c[i]);
+  delete[] a;
+  delete[] b;
+  delete[] c;
 }
