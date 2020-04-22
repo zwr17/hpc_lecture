27513cfc6d446f574b3e824466a1a11604bf2a30
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2020-04-22 09:42:36 +0900

    Update gang OpenACC

--- openacc/01_gang.cpp
+++ openacc/01_gang.cpp
@@ -2,13 +2,13 @@
 #include <openacc.h>
 
 int main() {
-#pragma acc parallel loop gang
+#pragma acc parallel loop gang num_gangs(8) vector_length(1)
   for(int i=0; i<2; i++) {
-    for(int i=0; i<5; i++) {
-      printf("%d %d %d: %d\n",
-             __pgi_vectoridx(),
-             __pgi_workeridx(),
-             __pgi_gangidx(),i);
+#pragma acc loop vector
+    for(int j=0; j<4; j++) {
+      printf("%d: %d, %d: %d\n",
+             __pgi_gangidx(),i,
+             __pgi_vectoridx(),j);
     }
   }
 }
