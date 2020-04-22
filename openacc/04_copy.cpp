d5a57046b98458e714cd256ccbcae688e64e9202
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2020-04-22 21:46:42 +0900

    OpenACC done

--- openacc/04_copy.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-
-int main(void) {
-  float *a = new float [4];
-#pragma acc parallel loop copyout(a[:4])
-  for (int i=0; i<4; i++) a[i] = i;
-  for (int i=0; i<4; i++) printf("%d %g\n",i,a[i]);
-  delete[] a;
-}
--- openacc/05_scope.cpp
+++ openacc/05_scope.cpp
@@ -1,13 +1,13 @@
 #include <cstdio>
-#include <omp.h>
+#include <openacc.h>
 
 int main() {
   int a = 0;
   int b[1] = {0};
-#pragma omp parallel for
+#pragma acc parallel loop //private(a)
   for(int i=0; i<8; i++) {
-    a = omp_get_thread_num();
-    b[0] = omp_get_thread_num();
+    a = __pgi_vectoridx();
+    b[0] = __pgi_vectoridx();
   }
   printf("%d %d\n",a,b[0]);
 }
