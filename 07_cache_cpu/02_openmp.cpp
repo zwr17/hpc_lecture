ca1c0a698e6030c778778468d450e9f78b0cabef
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2020-05-07 20:45:48 +0900

    Use vector and chrono

--- 07_cache_cpu/02_openmp.cpp
+++ 07_cache_cpu/02_openmp.cpp
@@ -1,24 +1,22 @@
 #include <cstdlib>
 #include <cstdio>
-#include <sys/time.h>
+#include <vector>
+#include <chrono>
+using namespace std;
 
 int main(int argc, char **argv) {
-  int N = atoi(argv[1]);
-  float ** A = new float * [N];
-  float ** B = new float * [N];
-  float ** C = new float * [N];
+  const int N = 2048;
+  vector<vector<float>> A(N,vector<float>(N));
+  vector<vector<float>> B(N,vector<float>(N));
+  vector<vector<float>> C(N,vector<float>(N));
   for (int i=0; i<N; i++) {
-    A[i] = new float [N];
-    B[i] = new float [N];
-    C[i] = new float [N];
     for (int j=0; j<N; j++) {
       A[i][j] = drand48();
       B[i][j] = drand48();
       C[i][j] = 0;
     }
   }
-  struct timeval tic, toc;
-  gettimeofday(&tic, NULL);
+  auto tic = chrono::steady_clock::now();
 #pragma omp parallel for
   for (int i=0; i<N; i++) {
     for (int k=0; k<N; k++) {
@@ -27,15 +25,7 @@ int main(int argc, char **argv) {
       }
     }
   }
-  gettimeofday(&toc, NULL);
-  double time = toc.tv_sec-tic.tv_sec+(toc.tv_usec-tic.tv_usec)*1e-6;
+  auto toc = chrono::steady_clock::now();
+  double time = chrono::duration<double>(toc - tic).count();
   printf("N=%d: %lf s (%lf GFlops)\n",N,time,2.*N*N*N/time/1e9);
-  for (int i=0; i<N; i++) {
-    delete[] A[i];
-    delete[] B[i];
-    delete[] C[i];
-  }
-  delete[] A;
-  delete[] B;
-  delete[] C;
 }
