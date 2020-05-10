f198c33ce12fff81d00a2fff7aa0333be6327c11
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2020-05-10 19:52:41 +0900

    Remove blislab

--- 07_cache_cpu/05_sgemm.cpp
+++ 07_cache_cpu/05_sgemm.cpp
@@ -8,7 +8,7 @@ using namespace std;
 extern "C" void sgemm_(char*, char*, int*, int*, int*, float*, float*, int*, float*, int*, float*, float*, int*);
 
 int main(int argc, char **argv) {
-  int N = 2048;
+  int N = 4096;
   vector<float> A(N*N);
   vector<float> B(N*N);
   vector<float> C(N*N);
@@ -19,10 +19,11 @@ int main(int argc, char **argv) {
       C[N*i+j] = 0;
     }
   }
-  auto tic = chrono::steady_clock::now();
   float alpha = 1.0;
   float beta = 0.0;
   char T = 'N';
+  sgemm_(&T, &T, &N, &N, &N, &alpha, &B[0], &N, &A[0], &N, &beta, &C[0], &N);
+  auto tic = chrono::steady_clock::now();
   sgemm_(&T, &T, &N, &N, &N, &alpha, &B[0], &N, &A[0], &N, &beta, &C[0], &N);
   auto toc = chrono::steady_clock::now();
   double time = chrono::duration<double>(toc - tic).count();
