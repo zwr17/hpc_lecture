40ec88addc908510b47d8404eb0ae4d77ba9ed3d
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2020-05-10 20:11:49 +0900

    Update CUDA matmul 00

--- 07_cache_gpu/00_base.cu
+++ 07_cache_gpu/00_base.cu
@@ -1,7 +1,8 @@
 #include <cmath>
 #include <cstdlib>
 #include <cstdio>
-#include <sys/time.h>
+#include <chrono>
+using namespace std;
 
 __global__ void matmul(float *A, float *B, float *C, int N) {
   int i = blockIdx.x;
@@ -12,55 +13,36 @@ __global__ void matmul(float *A, float *B, float *C, int N) {
 }
 
 int main(int argc, char **argv) {
-  int N = atoi(argv[1]);
-  float * h_A = new float [N*N];
-  float * h_B = new float [N*N];
-  float * h_C = new float [N*N];
-  float *d_A, *d_B, *d_C;
+  int N = 1024;
   int size = N * N * sizeof(float);
-  cudaMalloc((void **) &d_A, size);
-  cudaMalloc((void **) &d_B, size);
-  cudaMalloc((void **) &d_C, size);
+  float *A, *B, *C;
+  cudaMallocManaged(&A, size);
+  cudaMallocManaged(&B, size);
+  cudaMallocManaged(&C, size);
   for (int i=0; i<N; i++) {
     for (int j=0; j<N; j++) {
-      h_A[N*i+j] = drand48();
-      h_B[N*i+j] = drand48();
-      h_C[N*i+j] = 0;
+      A[N*i+j] = drand48();
+      B[N*i+j] = drand48();
+      C[N*i+j] = 0;
     }
   }
-  cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_C, h_C, size, cudaMemcpyHostToDevice);
-  struct timeval tic, toc;
-  gettimeofday(&tic, NULL);
-  matmul<<<N,N>>>(d_A, d_B, d_C, N);
+  auto tic = chrono::steady_clock::now();
+  matmul<<<N,N>>>(A, B, C, N);
   cudaDeviceSynchronize();
-  gettimeofday(&toc, NULL);
-  double time = toc.tv_sec-tic.tv_sec+(toc.tv_usec-tic.tv_usec)*1e-6;
+  auto toc = chrono::steady_clock::now();
+  double time = chrono::duration<double>(toc - tic).count();
   printf("N=%d: %lf s (%lf GFlops)\n",N,time,2.*N*N*N/time/1e9);
-  cudaMemcpy(h_A, d_A, size, cudaMemcpyDeviceToHost);
-  cudaMemcpy(h_B, d_B, size, cudaMemcpyDeviceToHost);
-  cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
-  gettimeofday(&tic, NULL);
-#pragma omp parallel for
-  for (int i=0; i<N; i++) {
-    for (int k=0; k<N; k++) {
-      for (int j=0; j<N; j++) {
-        h_C[N*i+j] -= h_A[N*i+k] * h_B[N*k+j];
-      }
-    }
-  }
-  gettimeofday(&toc, NULL);
-  time = toc.tv_sec-tic.tv_sec+(toc.tv_usec-tic.tv_usec)*1e-6;
-  printf("N=%d: %lf s (%lf GFlops)\n",N,time,2.*N*N*N/time/1e9);
-  float err = 0;
-  for (int i=0; i<N; i++) {
-    for (int j=0; j<N; j++) {
-      err += fabs(h_C[N*i+j]);
-    }
-  }
-  printf("error: %f\n",err/N/N);
-  delete[] h_A;
-  delete[] h_B;
-  delete[] h_C;
+  #pragma omp parallel for
+  for (int i=0; i<N; i++)
+    for (int k=0; k<N; k++)
+      for (int j=0; j<N; j++)
+        C[N*i+j] -= A[N*i+k] * B[N*k+j];
+  double err = 0;
+  for (int i=0; i<N; i++)
+    for (int j=0; j<N; j++)
+      err += fabs(C[N*i+j]);
+  printf("error: %lf\n",err/N/N);
+  cudaFree(A);
+  cudaFree(B);
+  cudaFree(C);
 }
