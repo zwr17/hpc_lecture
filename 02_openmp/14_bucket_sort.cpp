ebc183ec04fbd943b6eda12f4665b266c3a700da
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2020-05-27 21:07:13 +0900

    Fix typo

--- 02_openmp/14_bucket_sort.cpp
+++ 02_openmp/14_bucket_sort.cpp
@@ -20,7 +20,7 @@ int main() {
   std::vector<int> offset(range,0);
   for (int i=1; i<range; i++) 
     offset[i] = offset[i-1] + bucket[i-1];
-#pragma omp paralle for
+#pragma omp parallel for
   for (int i=0; i<range; i++) {
     int j = offset[i];
     for (; bucket[i]>0; bucket[i]--) {
