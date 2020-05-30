a22e3731c15cf7a559ae12443b2811577d61b503
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2020-05-30 10:36:10 +0900

    Update debugging examples

--- 10_debugger/02_matmul.cpp
+++ 10_debugger/02_matmul.cpp
@@ -8,7 +8,7 @@ typedef vector<vector<float>> matrix;
 void matmult(matrix &A, matrix &B, matrix &C, int N) {
   for (int i=0; i<N; i++)
     for (int k=0; k<N; k++)
-      for (int j=0; j<N; j++)
+      for (int j=0; j<N; k++)
         C[i][j] += A[i][k] * B[k][j];
 }
 
