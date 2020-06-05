22e42a66858b6b90d6e0a666a9351c0428d75a17
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2020-06-05 15:01:13 +0900

    completed timers

--- 11_profiler/timers.h
+++ 11_profiler/timers.h
@@ -9,16 +9,26 @@ clock_t elapse = 0;
 #elif GETTIMEOFDAY
 #include <sys/time.h>
 struct timeval start; 
-struct timeval stop;
 double elapse = 0;
 #elif BOOST
 #include <boost/timer/timer.hpp>
 boost::timer::cpu_timer timer;
 #elif CHRONO
 #include <chrono>
-using namespace std::chrono::steady_clock;
-time_point start;
-time_point stop;
+using namespace std::chrono;
+system_clock::time_point start;
+double elapse = 0;
+#elif GETTIME
+struct timespec start;
+double elapse = 0;
+#elif OMPGETWTIME
+#include <omp.h>
+double start;
+double elapse;
+#elif MPIWTIME
+#include <mpi.h>
+double start;
+double elapse;
 #else
 #error Define macro for timer type
 #endif
@@ -32,6 +42,14 @@ void startTimer() {
   gettimeofday(&start, NULL);
 #elif BOOST
   timer.start();
+#elif CHRONO
+  start = system_clock::now();
+#elif GETTIME
+  clock_gettime(CLOCK_REALTIME, &start);
+#elif OMPGETWTIME
+  start = omp_get_wtime();
+#elif MPIWTIME
+  start = MPI_Wtime();
 #endif
 }
 
@@ -41,10 +59,21 @@ void stopTimer() {
 #elif CLOCK
   elapse += clock() - start;
 #elif GETTIMEOFDAY
+  struct timeval stop;
   gettimeofday(&stop, NULL);
   elapse += stop.tv_sec-start.tv_sec+(stop.tv_usec-start.tv_usec)*1e-6;
 #elif BOOST
   timer.stop();
+#elif CHRONO
+  elapse += duration<double>(system_clock::now() - start).count();
+#elif GETTIME
+  struct timespec stop;
+  clock_gettime(CLOCK_REALTIME, &stop);
+  elapse += stop.tv_sec-start.tv_sec+(stop.tv_nsec-start.tv_nsec)*1e-9;
+#elif OMPGETWTIME
+  elapse += omp_get_wtime() - start;
+#elif MPIWTIME
+  elapse += MPI_Wtime() - start;
 #endif
 }
 
@@ -57,5 +86,13 @@ double getTime() {
   return elapse;
 #elif BOOST
   return (double)timer.elapsed().wall*1e-9;
+#elif CHRONO
+  return elapse;
+#elif GETTIME
+  return elapse;
+#elif OMPGETWTIME
+  return elapse;
+#elif MPIWTIME
+  return elapse;
 #endif
 }
