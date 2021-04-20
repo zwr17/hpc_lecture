f30e50b41449d82e9298f511b756d10e37364817
Rio Yokota <rioyokota@gsic.titech.ac.jp>
2021-04-20 21:22:12 +0900

    Revert homework answers

--- 04_simd/13_nbody.cpp
+++ 04_simd/13_nbody.cpp
@@ -1,7 +1,6 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
-#include <immintrin.h>
 
 int main() {
   const int N = 8;
@@ -12,33 +11,16 @@ int main() {
     m[i] = drand48();
     fx[i] = fy[i] = 0;
   }
-  __m256 zero = _mm256_setzero_ps();
-  for(int i=0; i<N; i+=8) {
-    __m256 xi = _mm256_load_ps(x+i);
-    __m256 yi = _mm256_load_ps(y+i);
-    __m256 fxi = zero;
-    __m256 fyi = zero;
+  for(int i=0; i<N; i++) {
     for(int j=0; j<N; j++) {
-      __m256 dx = _mm256_set1_ps(x[j]);
-      __m256 dy = _mm256_set1_ps(y[j]);
-      __m256 mj = _mm256_set1_ps(m[j]);
-      __m256 r2 = zero;
-      dx = _mm256_sub_ps(xi, dx);
-      dy = _mm256_sub_ps(yi, dy);
-      r2 = _mm256_fmadd_ps(dx, dx, r2);
-      r2 = _mm256_fmadd_ps(dy, dy, r2);
-      __m256 mask = _mm256_cmp_ps(r2, zero, _CMP_GT_OQ);
-      __m256 invR = _mm256_rsqrt_ps(r2);
-      invR = _mm256_blendv_ps(zero, invR, mask);
-      mj = _mm256_mul_ps(mj, invR);
-      invR = _mm256_mul_ps(invR, invR);
-      mj = _mm256_mul_ps(mj, invR);
-      fxi = _mm256_fmadd_ps(dx, mj, fxi);
-      fyi = _mm256_fmadd_ps(dy, mj, fyi);
+      if(i != j) {
+        float rx = x[i] - x[j];
+        float ry = y[i] - y[j];
+        float r = std::sqrt(rx * rx + ry * ry);
+        fx[i] -= rx * m[j] / (r * r * r);
+        fy[i] -= ry * m[j] / (r * r * r);
+      }
     }
-    _mm256_store_ps(fx+i, fxi);
-    _mm256_store_ps(fy+i, fyi);
-  }
-  for(int i=0; i<N; i++)
     printf("%d %g %g\n",i,fx[i],fy[i]);
+  }
 }
